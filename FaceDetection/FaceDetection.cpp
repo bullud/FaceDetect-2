3af3a32d5a76e16acd995a4274a4dc9bb0cf19fb
Jim Jin <jim_king_2000@163.com>
2014-05-27 21:58:00 +0800

    Revert "Revert "Add profile face detection""
    This reverts commit 60a7e86d51fe37ea5261e669fa046c9d8dc2e1ee.

--- FaceDetection/FaceDetection.cpp
+++ FaceDetection/FaceDetection.cpp
@@ -16,19 +16,27 @@ using cv::Rect;
 using cv::Size;
 using cv::Point;
 using cv::Scalar;
+using cv::ellipse;
 using std::wcerr;
 using std::vector;
 using std::for_each;
 
-void detect(CascadeClassifier &face_cascade, Mat &frame);
+void detect(CascadeClassifier &front_face_cascade, CascadeClassifier &profile_face_cascade, Mat &frame);
 
 int main(int argc, char** argv)
 {
-	CascadeClassifier face_cascade;
-	if (!face_cascade.load("Resource Files/haarcascade_frontalface_alt.xml"))
+	CascadeClassifier front_face_cascade;
+	if (!front_face_cascade.load("Resource Files/haarcascade_frontalface_alt.xml"))
 	{ 
-		wcerr << L"Cannot load face cascade file.\n"; 
+		wcerr << L"Cannot load front face cascade file.\n"; 
 		return -1; 
+	};
+
+	CascadeClassifier profile_face_cascade;
+	if (!front_face_cascade.load("Resource Files/haarcascade_profileface.xml"))
+	{
+		wcerr << L"Cannot load profile face cascade file.\n";
+		return -1;
 	};
 
 	CvCapture* capture = cvCaptureFromCAM(-1);
@@ -48,7 +56,7 @@ int main(int argc, char** argv)
 			return 2;
 		}
 
-		detect(face_cascade, frame);
+		detect(front_face_cascade, profile_face_cascade, frame);
 
 		//-- Show what you got
 		imshow("Face detection", frame);
@@ -63,20 +71,29 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-void detect(CascadeClassifier &face_cascade, Mat &frame)
+void detect(CascadeClassifier &front_face_cascade, CascadeClassifier &profile_face_cascade, Mat &frame)
 {
 	Mat frame_gray;
 	cvtColor(frame, frame_gray, CV_BGR2GRAY);
 	equalizeHist(frame_gray, frame_gray);
 
 	//-- Detect faces
-	std::vector<Rect> faces;
-	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+	vector<Rect> faces;
+	front_face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size{ 30, 30 });
 
 	for_each(faces.begin(), faces.end(), [&frame](const Rect &face)
 	{
-		Point center{ face.x + int(face.width * 0.5), face.y + int(face.height * 0.5) };
-		ellipse(frame, center, Size(face.width * 0.5, face.height * 0.5), 0, 0, 360, Scalar(0, 255, 0), 1, 8, 0);
+		Point center{ int(face.x + face.width * 0.5), int(face.y + face.height * 0.5) };
+		ellipse(frame, center, Size{ int(face.width * 0.5), int(face.height * 0.5) }, 0, 0, 360, Scalar{ 0, 255, 0 }, 1, 8, 0);
+	});
+
+	faces.clear();
+	profile_face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size{ 30, 30 });
+
+	for_each(faces.begin(), faces.end(), [&frame](const Rect &face)
+	{
+		Point center{ int(face.x + face.width * 0.5), int(face.y + face.height * 0.5) };
+		ellipse(frame, center, Size{ int(face.width * 0.5), int(face.height * 0.5) }, 0, 0, 360, Scalar{ 0, 255, 0 }, 1, 8, 0);
 	});
 }
 
