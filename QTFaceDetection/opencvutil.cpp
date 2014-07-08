2941a39bf1e8374a37126122fcc224d041a3aeda
Jim Jin <jim_king_2000@163.com>
2014-07-08 23:27:18 +0800

    Fix the color issue

--- QTFaceDetection/opencvutil.cpp
+++ QTFaceDetection/opencvutil.cpp
@@ -1,6 +1,8 @@
 #include "opencvutil.h"
+#include <opencv2/imgproc/imgproc.hpp>
 
 QImage OpenCVUtil::CVImgToQTImg(const cv::Mat &opencvImg)
 {
+    cv::cvtColor(opencvImg, opencvImg, CV_BGR2RGB);
     return QImage((const uchar*)opencvImg.data, opencvImg.cols, opencvImg.rows, QImage::Format_RGB888);
 }
